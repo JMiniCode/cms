2f2878e65d240e60ba86c482c4fa32880922ce22
JMenCode <jungle_men@mail.ru>
2018-08-13 03:44:10 +0400

    update app and router

--- src/App.php
+++ src/App.php
@@ -6,6 +6,24 @@ use JMCode\Http\{Request, Response};
 
 class App {
 
+    private $_setting = [];
+    private $_router;
+
+    /**
+     * @return Router
+     */
+    private function router($method = null, $args = []) {
+        if(!$this->_router) {
+            $this->_router = new Router();
+        }
+
+        if (!is_null($method)) {
+            call_user_func_array([$this->_router, $method], $args);
+        }
+
+        return $this->_router;
+    }
+
     /**
      * Create App
      * @param array $ops
@@ -21,11 +39,52 @@ class App {
     }
 
     /**
+     * @param string $name
+     * @param array $args
+     * @return void
+     */
+    public function __call($method, $args)
+    {
+        if (count($args) === 0) {
+            throw new \Exception('App::call - Аргументы не передана');
+        }
+
+        if($method === 'get' and count($args) === 1) {
+            return $this->_setting[$args[0]];
+        }
+
+        if (in_array($method, Router::ACCESS)) {
+            $offset = 0;
+            $call = $args[$offset];
+            $path = '*';
+            
+            if(!is_callable($call)) {
+                $ops = $call;
+
+                while (is_array($ops) && count($ops) !== 0) {
+                    $ops = $ops[0];
+                }
+
+                if (!is_callable($ops)) {
+                    $offset = 1;
+                    $path = $call;
+                }
+            }
+
+            $args = array_slice($args, $offset);
+            foreach($args AS $call) {
+                $this->router($method, [$path, $call]);
+            }
+        }
+    }
+
+    /**
      * Run App
-     *
      * @return void
      */
     public function run() {
-
+        echo '<pre>';
+        print_r($this->router());
+        echo '</pre>';
     }
 }
--- src/Router.php
+++ src/Router.php
@@ -3,5 +3,54 @@
 namespace JMCode\Cms;
 
 class Router {
-    
+
+    const ACCESS = ['get', 'post', 'use', 'all'];
+
+    private $_stack = [];
+
+    public function __call($method, $args) {
+        if (in_array($method, Router::ACCESS)) {
+            $offset = 0;
+            $call = $args[$offset];
+            $path = '*';
+
+            if(!is_callable($call)) {
+                $ops = $call;
+
+                while (is_array($ops) && count($ops) !== 0) {
+                    $ops = $ops[0];
+                }
+
+                if (!is_callable($ops)) {
+                    $offset = 1;
+                    $path = $call;
+                }
+            }
+
+            $args = array_slice($args, $offset);
+            foreach ($args AS $call) {
+                if(is_string($call) and file_exists($call)) {
+                    $call = require($call);
+                }
+
+                if($call instanceof self) {
+                    foreach($call->_stack AS $s) {
+                        $this->_stack[] = [
+                            $s[0],
+                            $path . $s[1],
+                            $s[2]
+                        ];
+                    }
+                }
+                
+                if(is_callable($call)) {
+                    $this->_stack[] = [
+                        $method,
+                        $path,
+                        $call
+                    ];
+                }
+            }
+        }
+    }
 }
